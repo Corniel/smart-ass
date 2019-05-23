8e6369c867528ae73895a76f5ba9f260b2e7fc12
Corniel Nobel <mail@corniel.nl>
2019-05-23 20:55:58 +0200

    Implemented Parser.ToDecimal()

--- src/SmartAss/Parser.cs
+++ src/SmartAss/Parser.cs
@@ -2,6 +2,16 @@
 {
     public static class Parser
     {
+        /// <summary>Parses a int.</summary>
+        /// <param name="str">
+        /// The input string.
+        /// </param>
+        /// <param name="n">
+        /// The parsed int.
+        /// </param>
+        /// <returns>
+        /// True if parsable, otherwise false.
+        /// </returns>
         public static bool ToInt32(string str, out int n)
         {
             n = default;
@@ -45,6 +55,16 @@
             return true;
         }
 
+        /// <summary>Parses a long.</summary>
+        /// <param name="str">
+        /// The input string.
+        /// </param>
+        /// <param name="n">
+        /// The parsed long.
+        /// </param>
+        /// <returns>
+        /// True if parsable, otherwise false.
+        /// </returns>
         public static bool ToInt64(string str, out long n)
         {
             n = default;
@@ -77,13 +97,105 @@
                     number *= 10;
                     number += ch - '0';
                 }
-                if ((number & 0x800000000000000) != 0)
+                if ((number & unchecked((long)0x8000000000000000)) != 0)
                 {
                     return false;
                 }
             }
 
             n = negative ? -number : number;
+            return true;
+        }
+
+        /// <summary>Parses a decimal.</summary>
+        /// <param name="str">
+        /// The input string.
+        /// </param>
+        /// <param name="dec">
+        /// The parsed decimal.
+        /// </param>
+        /// <returns>
+        /// True if parsable, otherwise false.
+        /// </returns>
+        public static bool ToDecimal(string str, out decimal dec)
+        {
+            dec = default;
+            if (string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+
+            var start = 0;
+            var end = str.Length;
+            var buffer = 0L;
+            var negative = false;
+            byte scale = 255;
+
+            int lo = 0;
+            int mid = 0;
+            var block = 0;
+
+            if (str[0] == '-')
+            {
+                start = 1;
+                negative = true;
+            }
+
+            for (var i = start; i < end; i++)
+            {
+                var ch = str[i];
+
+                // Not a digit.
+                if (ch < '0' || ch > '9')
+                {
+                    // if a dot and not found yet.
+                    if (ch == '.' && scale == 255)
+                    {
+                        scale = 0;
+                        continue;
+                    }
+                    return false;
+                }
+                unchecked
+                {
+                    buffer *= 10;
+                    buffer += ch - '0';
+
+                    // increase scale if found.
+                    if (scale != 255)
+                    {
+                        scale++;
+                    }
+                }
+
+                // Maximum decimals allowed is 28.
+                if (scale > 28)
+                {
+                    return false;
+                }
+                // No longer fits an int.
+                if ((buffer & 0xFFFF00000000) != 0)
+                {
+                    if (block == 0)
+                    {
+                        lo = unchecked((int)buffer);
+                    }
+                    else if (block == 1)
+                    {
+                        mid = unchecked((int)buffer);
+                    }
+                    // Does not longer fits block 2, so overflow.
+                    else if (block == 2)
+                    {
+                        return false;
+                    }
+                    buffer >>= 32;
+                    block++;
+                }
+            }
+
+            var hi = unchecked((int)buffer);
+            dec = new decimal(lo, mid, hi, negative, scale == 255 ? default : scale);
             return true;
         }
     }
--- test/SmartAss.UnitTests/ParserTest.cs
+++ test/SmartAss.UnitTests/ParserTest.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Globalization;
 using System.Linq;
+using Troschuetz.Random.Generators;
 
 namespace SmartAss.UnitTests
 {
@@ -14,23 +15,35 @@ namespace SmartAss.UnitTests
             Assert.AreEqual(0, result);
         }
 
+        [TestCase("8128472268774923738", 8128472268774923738L)]
+        public void ToInt64(string str, long expected)
+        {
+            Assert.IsTrue(Parser.ToInt64(str, out long actual));
+            Assert.AreEqual(expected, actual);
+        }
+
         [Test]
         public void ToInt32_10million_ShouldBeFaster()
         {
-            var numbers = 10 * 1000 * 1000;
-            var rnd = new Random(17);
+            var numbers = 2 * 1000 * 1000;
+            var rnd = new MT19937Generator(17);
 
             var strings = Enumerable.Range(0, numbers).Select(n => rnd.Next(int.MinValue, int.MaxValue).ToString()).ToArray();
 
-            Speed.Test(numbers, "int.TryParse()", (num) => int.TryParse(strings[num], NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out int result));
-            Speed.Test(numbers, "Converter.ToInt32()", (num) => Parser.ToInt32(strings[num], out int result));
+            Speed.Test(numbers, nameof(Int32TryParse), (num) => Int32TryParse(strings[num]));
+            Speed.Test(numbers, nameof(ParserInt32), (num) => ParserInt32(strings[num]));
+
+            for (var num = 0; num < numbers; num++)
+            {
+                Assert.AreEqual(Int32TryParse(strings[num]), ParserInt32(strings[num]));
+            }
         }
 
         [Test]
         public void ToInt64_10million_ShouldBeFaster()
         {
-            var numbers = 10 * 1000 * 1000;
-            var rnd = new Random(17);
+            var numbers = 2 * 1000 * 1000;
+            var rnd = new MT19937Generator(17);
 
             var strings = Enumerable.Range(0, numbers).Select(n =>
             {
@@ -40,8 +53,68 @@ namespace SmartAss.UnitTests
 
             }).ToArray();
 
-            Speed.Test(numbers, "int.TryParse()", (num) => long.TryParse(strings[num], NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out long result));
-            Speed.Test(numbers, "Converter.ToInt32()", (num) => Parser.ToInt64(strings[num], out long result));
+            Speed.Test(numbers, nameof(Int64TryParse), (num) => Int64TryParse(strings[num]));
+            Speed.Test(numbers, nameof(ParserInt64), (num) => ParserInt64(strings[num]));
+
+            for (var num = 0; num < numbers; num++)
+            {
+                Assert.AreEqual(Int64TryParse(strings[num]), ParserInt64(strings[num]));
+            }
+        }
+
+        [Test]
+        public void ToDecimal_10million_ShouldBeFaster()
+        {
+            var numbers = 2 * 1000 * 1000;
+            var rnd = new Random(17);
+
+            var strings = Enumerable.Range(0, numbers).Select(n =>
+            {
+                var f = (decimal)(2 * rnd.NextDouble() - 1);
+                var dec =  f * int.MaxValue;
+                return dec.ToString();
+            }).ToArray();
+
+            Speed.Test(numbers, nameof(DecimalTryParse), (num) => DecimalTryParse(strings[num]));
+            Speed.Test(numbers, nameof(ParserDecimal), (num) => ParserDecimal(strings[num]));
+
+            for (var num = 0; num < numbers; num++)
+            {
+                Assert.AreEqual(DecimalTryParse(strings[num]), ParserDecimal(strings[num]));
+            }
+        }
+
+        private static int ParserInt32(string str)
+        {
+            Parser.ToInt32(str, out int result);
+            return result;
+        }
+        private static long ParserInt64(string str)
+        {
+            Parser.ToInt64(str, out long result);
+            return result;
+        }
+        private static decimal ParserDecimal(string str)
+        {
+            Parser.ToDecimal(str, out decimal result);
+            return result;
+        }
+
+        private static int Int32TryParse(string str)
+        {
+            int.TryParse(str, NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out int result);
+            return result;
+        }
+
+        private static long Int64TryParse(string str)
+        {
+            long.TryParse(str, NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out long result);
+            return result;
+        }
+        private static decimal DecimalTryParse(string str)
+        {
+            decimal.TryParse(str, NumberStyles.AllowLeadingSign | NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out decimal result);
+            return result;
         }
     }
 }

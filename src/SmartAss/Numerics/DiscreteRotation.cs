9874bd5cab9f6a4a61925ee3c0e6f0190ea113b9
Corniel Nobel <mail@corniel.nl>
2020-12-24 23:36:46 +0100

    Updated sonar-analyzer.

--- src/SmartAss/Numerics/DiscreteRotation.cs
+++ src/SmartAss/Numerics/DiscreteRotation.cs
@@ -1,10 +1,23 @@
-﻿namespace SmartAss.Numerics
+﻿// <copyright file = "DiscreteRotation.cs">
+// Copyright (c) 2018-current, Corniel Nobel.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace SmartAss.Numerics
 {
+    /// <summary>Represents discrete rotations (counterclockwise).</summary>
     public enum DiscreteRotation
     {
+        /// <summary>0° (degrees)</summary>
         Deg000 = 0,
+
+        /// <summary>90° (degrees)</summary>
         Deg090 = 1,
+
+        /// <summary>180° (degrees)</summary>
         Deg180 = 2,
+
+        /// <summary>270° (degrees)</summary>
         Deg270 = 3,
     }
 }

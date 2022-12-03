ed8474034dece095958435bfc61acf48d86394ff
Corniel Nobel <mail@corniel.nl>
2022-12-03 07:38:26 +0100

    Add ChunkBy

--- src/SmartAss/Extensions/System.Linq.Enumerable.cs
+++ src/SmartAss/Extensions/System.Linq.Enumerable.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using SmartAss;
+using System.Collections.Generic;
 
 namespace System.Linq;
 
@@ -9,4 +10,23 @@ public static class SmartAssEnumerabelExtensions
         .Select((item, index) => new { item, index })
         .Where(pair => pair.index % step == 0)
         .Select(pair => pair.item);
+
+    /// <remarks>Only returns full chunks.</remarks>
+    public static IEnumerable<IReadOnlyList<TSource>> ChunkBy<TSource>(this IEnumerable<TSource> source, int groupSize)
+    {
+        Guard.NotNull(source, nameof(source));
+
+        var group = new TSource[groupSize];
+        var index = 0;
+        foreach (var item in source)
+        {
+            group[index++] = item;
+            if(index == groupSize)
+            {
+                yield return group;
+                index = 0;
+                group = new TSource[groupSize];
+            }
+        }
+    }
 }

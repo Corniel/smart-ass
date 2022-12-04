fd45537ea8c2d8954990d57324ffc65934c2d2d2
Corniel Nobel <mail@corniel.nl>
2022-12-04 10:22:27 +0100

    Introduction of Int32 Range.

---
+++ src/SmartAss/Conversion/Numerics/RangeConverter.cs
@@ -0,0 +1,17 @@
+﻿using SmartAss.Numerics;
+using System.ComponentModel;
+
+namespace SmartAss.Conversion.Numerics;
+
+public sealed class RangeTypeConverter : TypeConverter
+{
+    [Pure]
+    public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        => sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+
+    [Pure]
+    public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+       => value is string str
+        ? SmartAss.Numerics.Int32Range.Parse(str)
+        : base.ConvertFrom(context, culture, value);
+}

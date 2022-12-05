778eda80d3f061e2a759ec27988df73c76bd0479
Corniel Nobel <mail@corniel.nl>
2022-12-05 07:01:56 +0100

    CharPixels() on Multi-lines should also be able to read whitespace.

--- src/SmartAss/Parsing/StringParsing.cs
+++ src/SmartAss/Parsing/StringParsing.cs
@@ -86,7 +86,7 @@ namespace SmartAss.Parsing
             return new string(buffer, 0, length);
         }
 
-        public static CharPixels CharPixels(this IEnumerable<string> lines) => string.Join('\n', lines).CharPixels();
+        public static CharPixels CharPixels(this IEnumerable<string> lines, bool ignoreSpace = true) => string.Join('\n', lines).CharPixels(ignoreSpace);
 
         public static CharPixels CharPixels(this string str, bool ignoreSpace = true) => Parsing.CharPixels.Parse(str, ignoreSpace);
     }

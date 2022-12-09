ea15f628c5a132421f0b831ab817d285e0bdf0c6
Corniel Nobel <mail@corniel.nl>
2022-12-09 11:00:05 +0100

    Adds Numbers.Sign();

--- src/SmartAss/Numerics/Numbers.cs
+++ src/SmartAss/Numerics/Numbers.cs
@@ -1,8 +1,8 @@
-﻿using System;
-
-namespace SmartAss.Numerics;
+﻿namespace SmartAss.Numerics;
 
 public static class Numbers
 {
     public static int Sign(this int number) => Math.Sign(number);
+
+    public static int Abs(this int number) => Math.Abs(number);
 }

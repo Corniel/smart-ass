21d1702893b4ec9ea6fd2799dcef2cefb5025fe2
Corniel Nobel <mail@corniel.nl>
2022-12-13 10:36:55 +0100

    ComparesTo() an extension that returns nullable ints for a fluent way of multi ordering.

---
+++ src/SmartAss/ComparesTo.cs
@@ -0,0 +1,19 @@
+﻿namespace SmartAss;
+
+public static class ComparableExtensions
+{
+    public static int? ComparesTo<T>(this T? comparable, T? other) where T : struct, IComparable<T>
+    {
+        if (comparable is { } c && other is { } o) return c.ComparesTo(o);
+        else if (comparable.HasValue) { return -1; }
+        else if (other.HasValue) { return +1; }
+        else { return default; }
+    }
+
+    public static int? ComparesTo<T>(this T comparable, T other) where T : IComparable<T>
+    {
+        if (comparable is null) { return other is null ? null : +1; }
+        var compare = comparable.CompareTo(other);
+        return compare == 0 ? null : compare;
+    }
+}

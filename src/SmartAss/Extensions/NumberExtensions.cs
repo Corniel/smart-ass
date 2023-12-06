887117f8d44977ee29fca87c8f43445f36b65e98
Corniel Nobel <corniel@gmail.com>
2023-12-06 15:36:38 +0100

    Make extension methods for common maths functions.

--- src/SmartAss/Extensions/NumberExtensions.cs
+++ src/SmartAss/Extensions/NumberExtensions.cs
@@ -3,17 +3,50 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
+using System.Numerics;
+
 namespace System;
 
 public static class NumberExtensions
 {
     /// <summary>Gets the square of the number.</summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int Sqr(this int number) => number * number;
+    public static T Abs<T>(this T number) where T : struct, INumber<T>
+        => number < T.Zero ? -number : +number;
+
+    /// <summary>Gets the square of the number.</summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Sign<T>(this T number) where T : struct, INumber<T>
+    {
+        if(number == T.Zero) return 0;
+        else return number > T.Zero ? +1 : -1;
+    }
+
+    /// <summary>Gets the square of the number.</summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T Sqr<T>(this T number) where T: struct, INumberBase<T>
+        => number * number;
 
     /// <summary>Gets the discreet value of the square root of the number.</summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int Sqrt(this int number) => (int)Math.Sqrt(number);
+    public static double Sqrt(this double number) => Math.Sqrt(number);
+
+    /// <summary>Gets the discreet value of the square root of the number.</summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Sqrt(this int number) => Math.Sqrt(number);
+
+    /// <summary>Gets the discreet value of the square root of the number.</summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Sqrt(this long number) => Math.Sqrt(number);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Floor(this double number) => (int)Math.Floor(number);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Round(this double number) => (int)Math.Round(number);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Ceil(this double number) => (int)Math.Ceiling(number);
 
     public static IReadOnlyList<byte> Digits(this int number)
     {

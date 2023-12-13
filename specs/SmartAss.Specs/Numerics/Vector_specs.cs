6d37260d6ee988850ae13eba54fb46894c91a793
Corniel Nobel <corniel@gmail.com>
2023-12-13 22:02:36 +0100

    Rewritten some tests.

--- specs/SmartAss.Specs/Numerics/Vector_specs.cs
+++ specs/SmartAss.Specs/Numerics/Vector_specs.cs
@@ -10,24 +10,24 @@ public class Rotating_with
     [TestCase(+8)]
     public void multiples_of_4_rotations_do_not_change_vector(int rotation)
     {
-        Assert.AreEqual(Vector.N, Vector.N.Rotate((DiscreteRotation)rotation));
+        Vector.N.Rotate((DiscreteRotation)rotation).Should().Be(Vector.N);
     }
 
     [Test]
     public void one_step_rotates_counter_clock_whise()
     {
-        Assert.AreEqual(Vector.W, Vector.N.Rotate(DiscreteRotation.Deg090));
+         Vector.N.Rotate(DiscreteRotation.Deg090).Should().Be(Vector.W);
     }
 
     [Test]
     public void minus_one_step_rotates_clock_whise()
     {
-        Assert.AreEqual(Vector.E, Vector.N.Rotate((DiscreteRotation)(-1)));
+        Vector.N.Rotate((DiscreteRotation)(-1)).Should().Be(Vector.E);
     }
 
     [Test]
     public void two_steps_mirrors()
     {
-        Assert.AreEqual(Vector.S, Vector.N.Rotate(DiscreteRotation.Deg180));
+        Vector.N.Rotate(DiscreteRotation.Deg180).Should().Be(Vector.S);
     }
 }

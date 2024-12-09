4bf82e1da02cf916797a74055e0917f6458d1e4b
Corniel Nobel <corniel@gmail.com>
2024-12-10 00:30:40 +0100

    Round-robin on IEnumerables too.

--- src/SmartAss/Extensions/System.Linq.List.cs
+++ src/SmartAss/Extensions/System.Linq.List.cs
@@ -4,6 +4,11 @@ namespace System.Linq;
 
 public static class SmartAssListExtensions
 {
+    [OverloadResolutionPriority(-1)]
+    [Pure]
+    public static IEnumerable<Pair<T>> RoundRobin<T>(this IEnumerable<T> items) => items.ToArray().RoundRobin();
+
+    [OverloadResolutionPriority(1)]
     [Pure]
     public static IEnumerable<Pair<T>> RoundRobin<T>(this IReadOnlyList<T> list) => Pair.RoundRobin(list);
 }

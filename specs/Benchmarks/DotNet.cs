525c166f7ff347ea35379d04b4bf560dae94f2bc
Corniel Nobel <corniel@gmail.com>
2025-03-19 20:35:32 +0100

    wip

--- specs/Benchmarks/DotNet.cs
+++ specs/Benchmarks/DotNet.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
+
 namespace Benchmarks;
 
 public class DotNet
@@ -15,101 +18,60 @@ public class DotNet
         public class Abs
         {
             private const int Count = 1_000_000;
-            private readonly int[] I = new int[Count];
+            private readonly int[][] I =
+            [
+                new int[Count],
+                new int[Count],
+                new int[Count],
+            ];
             private readonly int[] O = new int[Count];
 
             public Abs()
             {
                 var rnd = new Random(42);
+                
                 for (int i = 0; i < Count; i++)
                 {
-                    I[i] = rnd.Next(-8000, 8001) * rnd.Next(8000);
+                    var factor = rnd.NextDouble() > 0.99 ? -1 : +1;
+
+                    I[0][i] = rnd.Next(-8000, 8001) * rnd.Next(8000);
+                    I[1][i] = +factor * rnd.Next(0, 8001) * rnd.Next(8000);
+                    I[2][i] = -factor * rnd.Next(0, 8001) * rnd.Next(8000);
                 }
             }
+
+            [Params(0)]
+            public int Set { get; set; }
 
             [Benchmark(Baseline = true)]
             public void Math_Abs()
             {
                 for (var i = 0; i < I.Length; i++)
                 {
-                    O[i] = Imp.Current(I[i]);
+                    O[i] = Imp.Current(I[Set][i]);
                 }
             }
 
             [Benchmark]
-            public void LT0()
+            public void Flatten()
             {
-                for (var i = 0; i < Count; i++)
+                for (var i = 0; i < I.Length; i++)
                 {
-                    O[i] = Imp.LT0(I[i]);
+                    O[i] = Imp.Flatten(I[Set][i]);
                 }
             }
 
             [Benchmark]
-            public void Not_Int_MinValue()
+            public void Alternative()
             {
-                for (var i = 0; i < Count; i++)
+                for (var i = 0; i < I.Length; i++)
                 {
-                    O[i] = Imp.NotIntMinValue(I[i]);
-                }
-            }
-
-            [Benchmark]
-            public void Unchecked()
-            {
-                for (var i = 0; i < Count; i++)
-                {
-                    O[i] = Imp.Unchecked(I[i]);
-                }
-            }
-
-            [Benchmark]
-            public void NoInlining()
-            {
-                for (var i = 0; i < Count; i++)
-                {
-                    O[i] = Imp.NoInlining(I[i]);
+                    O[i] = Imp.Alternative(I[Set][i]);
                 }
             }
 
             public static class Imp
             {
-                [MethodImpl(MethodImplOptions.AggressiveInlining)]
-                public static int LT0(int value)
-                {
-                    value = (value + (value >>= 31)) ^ value;
-
-                    if (value < 0)
-                    {
-                        throw new OverflowException("Some message");
-                    }
-                    return value;
-                }
-
-                [MethodImpl(MethodImplOptions.AggressiveInlining)]
-                public static int NotIntMinValue(int value)
-                {
-                    value = (value + (value >>= 31)) ^ value;
-
-                    if (value == int.MinValue)
-                    {
-                        throw new OverflowException("Some message");
-                    }
-                    return value;
-                }
-
-                [MethodImpl(MethodImplOptions.AggressiveInlining)]
-                public static int Unchecked(int value)
-                {
-                    value = unchecked((value + (value >>= 31)) ^ value);
-
-                    if (value == int.MinValue)
-                    {
-                        throw new OverflowException("Some message");
-                    }
-                    return value;
-                }
-
                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
                 public static int Current(int value)
                 {
@@ -118,23 +80,43 @@ public class DotNet
                         value = -value;
                         if (value < 0)
                         {
-                            throw new OverflowException("Some message");
+                            ThrowNegateTwosCompOverflow();
                         }
                     }
                     return value;
                 }
 
-                [MethodImpl(MethodImplOptions.NoInlining)]
-                public static int NoInlining(int value)
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                public static int Flatten(int value)
                 {
-                    value = value < 0 ? -value : value;
-
-                    if (value == int.MinValue)
+                    if (value < 0)
                     {
-                        throw new OverflowException("Some message");
+                        value = -value;
                     }
-
+                    if (value < 0)
+                    {
+                        ThrowNegateTwosCompOverflow();
+                    }
                     return value;
+                }
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                public static int Alternative(int value)
+                {
+                    value = unchecked((value + (value >>= 31)) ^ value);
+
+                    if (value < 0)
+                    {
+                        ThrowNegateTwosCompOverflow();
+                    }
+                    return value;
+                }
+
+                [DoesNotReturn]
+                [StackTraceHidden]
+                internal static void ThrowNegateTwosCompOverflow()
+                {
+                    throw new OverflowException("Message");
                 }
             }
         }
